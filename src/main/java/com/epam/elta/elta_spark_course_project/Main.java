10b39631c9b4cda28443389061be4d1fd4955c00
gennadyzaytsev <gennadyza@papayaglobal.com>
2022-12-01 15:53:47 +0200

    part1 task completed

--- src/main/java/com/epam/elta/elta_spark_course_project/Main.java
+++ src/main/java/com/epam/elta/elta_spark_course_project/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder()
                 .appName("spark streaming")
-                .config("spark.master", "local[*]")
+                .config("spark.master", "local")
                 .getOrCreate();
 
         spark.sparkContext().setLogLevel("ERROR");
